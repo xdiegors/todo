978b93d7f7446e590067d6a743e9a4213670148f
xdiegors <xdiegors@outlook.com>
2022-07-17 10:39:32 -0300

    Interpolation, ngFor and event binding test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: any[] = [];
+  public title: string = 'Tasks';
+
+  constructor() {
+    this.todos.push('Buy milk');
+    this.todos.push('Watch TV');
+    this.todos.push('Play Fortnite');
+  }
+
+  changeText() {
+    this.title = 'New Title';
+  }
 }
